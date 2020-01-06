64d9ae116dc7fca4dd8b897a60c12b3c051bf07f
Mzito <you@example.com>
2020-01-06 13:31:02 +0200

    commit

---
+++ spec/memory-game.spec.js
@@ -0,0 +1,8 @@
+let game = require('../src/script');
+
+describe("memory game", function() {
+    
+    it("displays victory message", function () {
+        expect(game.checkMatch()).toBe();
+    });
+});

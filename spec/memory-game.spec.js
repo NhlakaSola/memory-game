6c0e418ea97673fc1bb10413f54846ccd1a260c2
Mzito <you@example.com>
2020-01-15 16:03:10 +0200

    changed the game still no specs

--- spec/memory-game.spec.js
+++
@@ -1,8 +0,0 @@
-let game = require('../src/script');
-
-describe("memory game", function() {
-    
-    it("displays victory message", function () {
-        expect(game.checkMatch()).toBe();
-    });
-});
--- src/script.js
+++ src/script.js
@@ -1,85 +1,105 @@
-const cards = document.querySelectorAll('.memory-card');
+let tileImages = [];
+let tileArray = [];
+let tileFlippedOver = [];
+let cardFlipped = -1;
+let timer = '';
+var playLockout = false;
+let startButton = document.getElementById('start');
+let gameBoard = document.getElementById('gameBoard');
+let gamePlay = false;
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard; 
-let count = 0;
 
-function flipCard(){
-    if (lockBoard) return;
-    if (this === firstCard) return
+startButton.addEventListener('click',startGame);
 
-    this.classList.add('flip');
 
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-        return;
-    }
-
-    hasFlippedCard = false;
-    secondCard = this;
-    checkMatch();
-
-}
-
-function checkMatch(){
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        disableCards();
-        count++;
-    } else {
-        unFlipCards();    
-    }
-    if (count == 6) {
-        setTimeout(() => {
-            alert("Won!!!");
-        }, 500)
+function startGame(){
+    cardFlipped = -1;
+    playLockout = false;
+    startButton.style.display='none';
+    if(!gamePlay){
+        gamePlay = true;
+        buildArray();
+        tileArray = tileImages.concat(tileImages);
+        shuffleArray(tileArray);
+        buildBoard();
+        
     }
 }
 
-function disableCards(){
-    firstCard.removeEventListener('click', flipCard);       
-    secondCard.removeEventListener('click', flipCard); 
-    resetBoard();
-
+function buildArray(){
+    for (let i = 1; i < 7; i++){
+        tileImages.push(i+'.png');
+      } 
 }
 
-function unFlipCards() {
-    lockBoard = true;
-    setTimeout(() =>{
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-        resetBoard();
-    }, 800);
-}
-
-function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false,false];
-    [firstCard, secondCard]=[null,null];
-}
-
-
-(function shuffle(){
-    cards.forEach( cards => {
-        let shuffler = Math.floor(Math.random() * 12);
-        cards.style.order = shuffler;
+function buildBoard(){
+    var html = "";
+    for (var x = 0; x <= (tileArray.length -1); x++) {
+        html += '<div class = "gameTile"><div class = "gameTile">';
+        html += '<img id="cards' + x + '" src="img/back.png" onclick="pickCard(' + x + ',this)" class="flipImage"></div></div>';  
     }
-
-    )
-})();
-
-function restart(){
-    var okToRefresh = confirm("Do you want to play again?");
-    if (okToRefresh){
-        setTimeout("location.reload(true);",500);
-        shuffle();
-    }
- 
+    gameBoard.innerHTML = html;
 }
 
+function pickCard(tileIndex,t) {
+    if (!inArray(t.id,tileFlippedOver) && !playLockout) {
+        if (cardFlipped >=0) {
+            cardFlip(t,tileIndex);
+            playLockout = true;
+        if (checkSrc(tileFlippedOver[tileFlippedOver.length-1]) == checkSrc(tileFlippedOver[tileFlippedOver.length-2])) {
+            playLockout = false;
+            cardFlipped = -1;
+            if (tileFlippedOver.length == tileArray.length) {
+                gameOver();
+            }
+        }else{
+            timer = setInterval(hideCard, 1000);
+        }
+    }else{
+        cardFlipped = tileIndex;
+        cardFlip(t,tileIndex); 
+    }
+   }
+}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function hideCard() {
+    for (let x = 0; x < 2; x++) {
+        let vid = tileFlippedOver.pop();
+        document.getElementById(vid).src = "img/back.png" 
+    }
+    clearInterval(timer);
+    playLockout = false;
+    cardFlipped = -1;
+}
 
-module.exports = {
-    restart,shuffle, flipCard,unFlipCards,resetBoard,disableCards
+function gameOver() {
+    startButton.style.display = 'block';
+    gamePlay = false;
+    tileImages = [];
+    tileFlippedOver = [];
+    setTimeout(function(){ alert("Click start for new game"); }, 200);
+  }
+
+function inArray(x,array) {
+    return array.indexOf(x) > -1;
+}
+
+function cardFlip(t,ti) {
+    t.src = "img/"+tileArray[ti];
+    tileFlippedOver.push(t.id);
+}
+
+function checkSrc(v) {
+    var v = document.getElementById(v).src;
+    return v;
+}
+
+function shuffleArray(array){
+    for (let j = array.length - 1; j >0; j--) {
+        let holder = Math.floor(Math.random() * (j+1));
+        var itemValue = array[j];
+        array[j] = array[holder];
+        array[holder] = itemValue;
+    }
+    return array;
 }
